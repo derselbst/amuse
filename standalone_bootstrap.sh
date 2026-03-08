c7633981515f2b988bceeff5dd119e475686b530
derselbst <tom.mbrt@googlemail.com>
2026-03-08 13:15:22 +0100

    Workaround build issues

--- standalone_bootstrap.sh
+++ standalone_bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+# MAKE SURE CLANG13-devel is installed - newer clang will NOT WORK!!!
+
 git clone https://github.com/AxioDL/boo.git
 (cd boo && git submodule update --recursive --init)
 
 git clone https://github.com/libAthena/athena.git
-(cd athena && git submodule update --recursive --init)
+(cd athena && git checkout f7c1cd8f59e522651b6cf1f8df011028940abee3~1 && git submodule update --recursive --init)
